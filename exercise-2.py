4c0b83f88cfb8980b8ddf890015ea198c24e1680
= <afrbernardino@gmail.com>
2016-12-08 19:23:14 +0000

    2

--- exercise-2.py
+++ exercise-2.py
@@ -1,1 +1,20 @@
-print "hello"
+from sklearn.feature_extraction.text import TfidfVectorizer
+from stop_words import get_stop_words
+import os
+
+# fetching train data/collenction from disk
+# using wiki20 collection from https://github.com/zelandiya/keyword-extraction-datasets
+train = []
+for file in os.listdir("wiki20/documents"):
+    if file.endswith(".txt"):
+        with open("wiki20/documents/" + file, 'r') as myfile:
+            train.append(myfile.read().replace('\n', ''))
+            
+# get stop words
+stop_words = get_stop_words('en')
+
+# fetch words
+vectorizer = TfidfVectorizer(use_idf=False, ngram_range=(1, 3), stop_words=stop_words)
+
+# learn idf
+trainvec = vectorizer.fit_transform(train)
