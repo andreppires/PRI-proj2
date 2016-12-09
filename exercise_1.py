01c665081c25fea309d9edbcf0c00db5f4993042
AFBernardino <afrbernardino@gmail.com>
2016-12-09 19:12:56 +0000

    fix I hope

--- exercise_1.py
+++ exercise_1.py
@@ -1,54 +1,53 @@
 from stop_words import get_stop_words
-from sklearn.feature_extraction.text import TfidfVectorizer
+from nltk import ngrams
 import operator
-import os
+import string
 
-# return feature names from file using tf-idf algorithm
-def get_feature_names(file_name, file_content):
-	print "Getting feature names..."
-
+# return new content witthout punctuation and stop_words
+def clean_content(content):
 	# get stop words
 	stop_words = get_stop_words("en")
 
-	# fetch train data/collenction from disk
-	# using wiki20 collection from https://github.com/zelandiya/keyword-extraction-datasets
-	train = []
-	for file in os.listdir("wiki20/documents"):
-		if file.endswith(".txt"):
-		    with open("wiki20/documents/" + file, 'r') as myfile:
-		        train.append(myfile.read().replace('\n', ''))
+	# erase ponctuation
+	content = "".join(ch for ch in content if ch not in string.punctuation)
 
-	# tri-gramas and no stop-words
-	vectorizer = TfidfVectorizer(use_idf=False, ngram_range=(1, 3)
-	, stop_words=stop_words)
+	# erase stop words
+	content  = [word for word in content.split() if word.lower() not in stop_words]
 
-	# learn idf
-	# carefull here. more document -> more precision -> more time to compute them all
-	trainvec = vectorizer.fit_transform(train[:2])
+	return " ".join(content)
 
-	# input document as array
-	input_document = []
+# return candidates for file_name or file_content
+def get_candidates(file_name, file_content, small_n_gram, high_n_gram):
+	print "Getting candidates..."
+
+	# content to work with
+	new_content = ""
 
 	# read file
 	if (file_name is not None):
 		with open(file_name, "r") as f:
 			content = f.read()
-			input_document.append(content)
+			new_content = clean_content(content);
 			f.close()
 	else:
-		input_document.append(file_content)
+		new_content = clean_content(file_content)
 
-	# apply tf
-	testvec = vectorizer.transform(input_document)
+	# candidates
+	candidates = []
 
-	# get feature names
-	feature_names = vectorizer.get_feature_names()
+	# get candidates (n grams)
+	for n in range(small_n_gram, high_n_gram + 1):
+		n_grams = ngrams(new_content.split(), n)
+		for grams in n_grams:
+			candidate = " ".join(grams)
+			candidates.append(candidate)
 
-	return feature_names
+	return list(set(candidates))
 
 # return page rank for each keyphrase
 def get_page_rank(keyphrases):
 	print "Getting page ranks..."
+	
 	keywords = []
 
 	# clean unicode
@@ -60,7 +59,7 @@ def get_page_rank(keyphrases):
 	for x in keywords:
 		PR.setdefault(x, [])
 		PR[x].append(1)
-
+		
 	# find links
 	x = []
 	for u in keywords:
@@ -91,8 +90,8 @@ def get_page_rank(keyphrases):
 
 # get top 5 keyphrases from file_name or file_content using PageRank algorithm
 def get_top_five(file_name, file_content):
-	fn = get_feature_names(file_name, file_content)
-	pr = get_page_rank(fn)
+	candidates = get_candidates(file_name, file_content, 1, 3)
+	pr = get_page_rank(candidates)
 
 	print "Getting top five..."
 	top_five = []
